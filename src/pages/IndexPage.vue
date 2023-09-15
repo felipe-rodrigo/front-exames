525462629a1115f1495edf0148de1d7bd064ae22
felipe-rodrigo <feliperodrigoespindola@gmail.com>
2023-09-15 18:17:09 -0400

    Atualização FrontEnd

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-page padding>
+    <DashboardTest />
+    testetstets
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import DashboardTest from 'src/dashboard/DashboardTest.vue'
+
+export default defineComponent({
+
+  name: 'IndexPage',
+  components: {
+    DashboardTest
+  }
+})
+</script>
